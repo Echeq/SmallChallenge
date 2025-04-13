c3f8f2ca271c1a3163a47c0308205c3ddbae29a2
Echeq <3659397757@qq.com>
2025-04-13 22:51:36 +0800

    Comments and Reflexions in comments

--- Python/API PRACTICES/1. MyFirstApi/main.py
+++ Python/API PRACTICES/1. MyFirstApi/main.py
@@ -1,3 +1,21 @@
+'''
+Para ejecutar FLASK es recomendable ejecutarlo en un entorno virtual, aca les dejo el proceso par hacerlo:
+1. En la misma carpeta escribes ''python3 -m venv mi_entorno'', en mi_entorno puedes poner otro nombre.
+2. Luego para activar el entorno escribes "source mi_entorno/bin/activate", en el mi_entorno", reemplazas
+    por el nombre que le pusiste al entorno de Flask.
+3. Cuando vayas a correr la API, debes hacerlo desde la misma consola, si lo corres con el boton de VSCode
+    puede que te de error. Recomendable hacer "python nombre_de_la_aplicacion.py"
+
+EXTRA:
+1. En VSCode Existe POSTMAN, puedes instalarlo mejor en VSCode si tu sistema operativo es linux y no quieres
+    enrredarte.
+2. Recuerda configurar bien la peticion POST en POSTMAN ya sea javascript, JSON, HTML, XML, etc.
+pd: Elvis, estuviste 30 minutos viendo el error y te diste cuenta cuando maximisaste la pantalla.
+
+Hasta aca llega el reporte del dia de hoy.
+'''
+
+
 #La cabezera importante
 from flask import Flask, jsonify, request
 app = Flask(__name__)
